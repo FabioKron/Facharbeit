52e2a5a2ea908325378368e7a457ad1393891d5c
FabioKron <fabio.kron@web.de>
2021-01-29 16:32:41 +0100

    Hinzufügen von hinzufuegen.py: Möglichkeit zur Analyse des Beispiels hinzufügen. Die Werte werden in einer .csv-Datei gespeichert

---
+++ Anwendungsfaelle/test_hinzufuegen/hinzufuegen.py
@@ -0,0 +1,45 @@
+import time
+import numpy
+import random
+import sys
+
+
+if __name__ == '__main__':
+
+    #Erstellen der Datei
+    args = sys.argv
+    filename = sys.argv[1]
+    open(filename, "x").close()
+
+    with open(filename, "a") as file:
+        file.write(",".join(["Anzahl Elemente", "Speicher Liste", "Speicher Array", "Laufzeit Liste","Laufzeit Array"])+"\n")
+
+    # Erstellen der Liste und des Arrays
+    liste = []
+    array = numpy.array([], dtype=float)
+
+    # Initalisieren der Variablen, die die Zeit speichern
+    array_time = 0
+    list_time = 0
+
+    # Zählvariable, wird benötigt um alle 10000 Elemente zu speichern
+    i = 0
+
+    while True:
+        zahl = random.random()
+
+        start_of_next_array_timeframe = time.time()
+        array = numpy.append(array, zahl)
+        array_time += time.time() - start_of_next_array_timeframe
+
+        start_of_next_list_timeframe = time.time()
+        liste.append(zahl)
+        list_time += time.time() - start_of_next_list_timeframe
+
+        i += 1
+
+        if not i % 10000:
+            with open(filename, "a") as file:
+                file.write(",".join([str(i), str(sys.getsizeof(liste)), str(sys.getsizeof(array)), str(list_time), str(array_time)])+"\n")
+                print("Status für", i, " Elemente gespeichert.")
+
