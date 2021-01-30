f1c540e9f8d3cc541314d3abdb219e6d4d7e72be
FabioKron <fabio.kron@web.de>
2021-01-30 07:58:02 +0100

    Umwandeln der Daten in Liste Die bereits ausgelesenen Daten der eingelesenen Datei werden in eine besser benutzbare Liste umgewandelt

--- Anwendungsfaelle/test_hinzufuegen/memory_plotting.py
+++ Anwendungsfaelle/test_hinzufuegen/memory_plotting.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
 import sys
 
-#Einlesen der Datei
+# Einlesen der Datei
 with open(str(sys.argv[1]), "r") as file:
     file_data = file.read()
+
+# Umwandeln der Daten zu Liste
+file_data = file_data.split("\n")
+file_data = [row.split(",") for row in file_data]
