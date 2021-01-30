059856032bea855531b3b8e6d7f8b6b9aa8d767e
FabioKron <fabio.kron@web.de>
2021-01-30 08:15:44 +0100

    Grafische Darstellung der Daten Durch das Modul matplotlib.pyplot werden die Daten nun grafisch dargestellt

--- Anwendungsfaelle/test_hinzufuegen/memory_plotting.py
+++ Anwendungsfaelle/test_hinzufuegen/memory_plotting.py
@@ -9,6 +9,13 @@ with open(str(sys.argv[1]), "r") as file:
 file_data = file_data.split("\n")
 file_data = [row.split(",") for row in file_data if row]
 
+# Sortieren der Daten in seperate Listen
 iterations = [int(row[0]) for row in file_data[1:]]
 memory_list = [int(row[1]) for row in file_data[1:]]
 memory_array = [int(row[2]) for row in file_data[1:]]
+
+# Grafische Darstellung der Listen
+plt.plot(iterations, memory_list)
+plt.plot(iterations, memory_array)
+
+plt.show()
