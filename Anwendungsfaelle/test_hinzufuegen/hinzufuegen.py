d99171143c4ab02ac95f31aab4df9e3fe2f7837b
FabioKron <fabio.kron@web.de>
2021-02-07 08:53:44 +0100

    Änderungen in hinzufuegen.py: - Ändern der Sprache des Codes  und der Ausgaben zu Englisch - Ausgeben der Nachricht, wie viele Durchläufe es gab nur alle 1000 Elemente - Autoformatieren des Quelltexts

--- Anwendungsfaelle/test_hinzufuegen/hinzufuegen.py
+++ Anwendungsfaelle/test_hinzufuegen/hinzufuegen.py
@@ -3,20 +3,21 @@ import numpy
 import random
 import sys
 
-
 if __name__ == '__main__':
 
-    #Erstellen der Datei
+    # Erstellen der Datei
     args = sys.argv
     filename = sys.argv[1]
     open(filename, "x").close()
 
     with open(filename, "a") as file:
-        file.write(",".join(["Anzahl Elemente", "Speicher Liste", "Speicher Array", "Laufzeit Liste","Laufzeit Array"])+"\n")
+        file.write(",".join(
+            ["iterations", "memory list", "memory array", "execution time list", "execution time array"]
+        ) + "\n")
 
     # Erstellen der Liste und des Arrays
-    liste = []
-    array = numpy.array([], dtype=float)
+    list_to_track = []
+    array_to_track = numpy.array([], dtype=float)
 
     # Initalisieren der Variablen, die die Zeit speichern
     array_time = 0
@@ -32,20 +33,23 @@ if __name__ == '__main__':
     i = 0
 
     while i < iterations:
-        zahl = random.random()
+        randomNumber = random.random()
 
         start_of_next_array_timeframe = time.time()
-        array = numpy.append(array, zahl)
+        array_to_track = numpy.append(array_to_track, randomNumber)
         array_time += time.time() - start_of_next_array_timeframe
 
         start_of_next_list_timeframe = time.time()
-        liste.append(zahl)
+        list_to_track.append(randomNumber)
         list_time += time.time() - start_of_next_list_timeframe
 
         i += 1
 
         # Speichern der Messwerte in der Datei
         with open(filename, "a") as file:
-            file.write(",".join([str(i), str(sys.getsizeof(liste)), str(sys.getsizeof(array)), str(list_time), str(array_time)])+"\n")
-            print("Status für", i, " Elemente gespeichert.")
+            file.write(",".join(
+                [str(i), str(sys.getsizeof(list_to_track)), str(sys.getsizeof(array_to_track)), str(list_time),
+                 str(array_time)]) + "\n")
 
+        if not i % 1000:
+            print("Status saved for", i, " iteration")
