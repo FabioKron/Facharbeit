c3010e6804176b963edfaa1fc12a663b5f5d2db1
FabioKron <fabio.kron@web.de>
2021-02-10 09:08:19 +0100

    Vereinfachen der Programmlogik: Entfernen der Abfrage __name__ == "__main__"

--- Anwendungsfaelle/test_anhaengen/hinzufuegen.py
+++ Anwendungsfaelle/test_anhaengen/hinzufuegen.py
@@ -3,53 +3,51 @@ import numpy
 import random
 import sys
 
-if __name__ == '__main__':
+# Erstellen der Datei
+args = sys.argv
+filename = sys.argv[1]
+open(filename, "x").close()
 
-    # Erstellen der Datei
-    args = sys.argv
-    filename = sys.argv[1]
-    open(filename, "x").close()
+with open(filename, "a") as file:
+    file.write(",".join(
+        ["iterations", "memory list", "memory array", "execution time list", "execution time array"]
+    ) + "\n")
 
+# Erstellen der Liste und des Arrays
+list_to_track = []
+array_to_track = numpy.array([], dtype=float)
+
+# Initalisieren der Variablen, die die Zeit speichern
+array_time = 0
+list_time = 0
+
+# Festlegen der Anzahl der Durchläufe
+if len(sys.argv) > 2:
+    iterations = int(sys.argv[2])
+else:
+    iterations = 10000
+
+# Zählvariable, wird benötigt um alle Elemente zu speichern
+i = 0
+
+while i < iterations:
+    randomNumber = random.random()
+
+    start_of_next_array_timeframe = time.time()
+    array_to_track = numpy.append(array_to_track, randomNumber)
+    array_time += time.time() - start_of_next_array_timeframe
+
+    start_of_next_list_timeframe = time.time()
+    list_to_track.append(randomNumber)
+    list_time += time.time() - start_of_next_list_timeframe
+
+    i += 1
+
+    # Speichern der Messwerte in der Datei
     with open(filename, "a") as file:
         file.write(",".join(
-            ["iterations", "memory list", "memory array", "execution time list", "execution time array"]
-        ) + "\n")
+            [str(i), str(sys.getsizeof(list_to_track)), str(sys.getsizeof(array_to_track)), str(list_time),
+             str(array_time)]) + "\n")
 
-    # Erstellen der Liste und des Arrays
-    list_to_track = []
-    array_to_track = numpy.array([], dtype=float)
-
-    # Initalisieren der Variablen, die die Zeit speichern
-    array_time = 0
-    list_time = 0
-
-    # Festlegen der Anzahl der Durchläufe
-    if len(sys.argv) > 2:
-        iterations = int(sys.argv[2])
-    else:
-        iterations = 10000
-
-    # Zählvariable, wird benötigt um alle Elemente zu speichern
-    i = 0
-
-    while i < iterations:
-        randomNumber = random.random()
-
-        start_of_next_array_timeframe = time.time()
-        array_to_track = numpy.append(array_to_track, randomNumber)
-        array_time += time.time() - start_of_next_array_timeframe
-
-        start_of_next_list_timeframe = time.time()
-        list_to_track.append(randomNumber)
-        list_time += time.time() - start_of_next_list_timeframe
-
-        i += 1
-
-        # Speichern der Messwerte in der Datei
-        with open(filename, "a") as file:
-            file.write(",".join(
-                [str(i), str(sys.getsizeof(list_to_track)), str(sys.getsizeof(array_to_track)), str(list_time),
-                 str(array_time)]) + "\n")
-
-        if not i % 1000:
-            print("Status saved for", i, " iteration")
+    if not i % 1000:
+        print("Status saved for", i, " iteration")
