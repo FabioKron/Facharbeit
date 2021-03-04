5e6a3a09d09d4025baf3edf98d8faabd17ddb1fa
FabioKron <fabio.kron@web.de>
2021-03-04 10:15:52 +0100

    Hinzufügen eines Skripts, um automatisiert mehrere Testdurchläufe zu machen

---
+++ Anwendungsfaelle/test_anhaengen/track_anhaengen_float.py
@@ -0,0 +1,12 @@
+import anhaengen_float
+
+iterations = int(input("Iterations for each run: "))
+numOfTestFiles = int(input("Number of test runs: "))
+filenames = input("Filename without type-end to save data: ")
+
+current_run = 0
+while current_run < numOfTestFiles:
+    current_run += 1
+    print("starting run", current_run, "...")
+    anhaengen_float.gen_test_data(filenames + str(current_run) + ".csv", iterations)
+    print("finished run", current_run, "...")
