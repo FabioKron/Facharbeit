e0ccfb74fbeed7dfb9b7285d8e83cdec64bfda0b
FabioKron <fabio.kron@web.de>
2021-03-06 10:50:57 +0100

    Ersetzen runtime_plotting > plotter_gesamtlaufzeit Ersetzen von runtime_plotting.py plotter_gesamtlaufzeit.py Das alte Skript war ungültig durch das Speichern von mehr Informationen + das neue Skript ist in deutsch

---
+++ Anwendungsfaelle/test_anhaengen/plotter_gesamtlaufzeit.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import sys
+
+# Einlesen der Datei als String
+with open(str(sys.argv[1]), "r") as datei:
+    dateiinhalt: str = datei.read()
+
+# Umwandeln des Dateiinhalts von String in Liste
+dateiinhalt: list = dateiinhalt.split("\n")
+dateiinhalt: list = [zeile.split(",") for zeile in dateiinhalt if zeile]
+
+# Sortieren der benoetigten Daten in seperate Listen
+wiederholung: list = [int(zeile[0]) for zeile in dateiinhalt[1:]]
+gesamtlaufzeit_liste: list = [float(zeile[5]) for zeile in dateiinhalt[1:]]
+gesamtlaufzeit_array: list = [float(zeile[6]) for zeile in dateiinhalt[1:]]
+
+# Grafische Darstellung der Listen
+liste, = plt.plot(wiederholung, gesamtlaufzeit_liste, label="Liste")
+array, = plt.plot(wiederholung, gesamtlaufzeit_array, label="Array")
+
+plt.margins(0)
+
+plt.title(sys.argv[1] + "\nGesamtlaufzeit nach Anzahl der Ausführungen")
+plt.xlabel("Anzahl der Ausführungen")
+plt.ylabel("Gesamtlaufzeit in Sekunden")
+
+plt.subplots_adjust(left=0.15)
+
+plt.legend((liste, array), ("Liste", "Array"))
+
+plt.show()
